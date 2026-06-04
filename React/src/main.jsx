031bc37b56c67781e2194b827f24bdc8e8df5021
shareedhar <shreedharniki@gmail.com>
2026-06-05 03:06:13 +0530

    code clean in main .js

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import "./index.css"; // if you’re using Tailwind or custom CSS
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
 
 import React from "react";
 import ReactDOM from "react-dom/client";
